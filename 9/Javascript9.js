37e093086025ed3923a5803e8af59d5011daf466
TheFoxerThatFoxed <214658836+UnFoxable@users.noreply.github.com>
2026-04-24 16:57:51 +0800

    Add files via upload

--- 9/Javascript9.js
+++ 9/Javascript9.js
@@ -1,19 +1,18 @@
-var rcanvas = document.getElementById("referenceimg");
-var rctx = rcanvas.getContext('2d', {alpha: true, willReadFrequently: true});
-console.log(rctx.getContextAttributes());
 var dcanvas = document.getElementById("displayimg");
 var dctx = dcanvas.getContext('2d');
+
+var rcanvas = new OffscreenCanvas(dcanvas.clientWidth*2, dcanvas.clientHeight*2);
+var rctx = rcanvas.getContext('2d', {alpha: true, willReadFrequently: true});
+console.log(rctx.getContextAttributes());
+
 rctx.imageSmoothingEnabled = true;
+var intsize = 2;
 resize();
 
-
+var nextline = [];
 function draw() {
     color = document.querySelector('#pickcolour').value;
-
     inptwidth = document.querySelector('#inptwidth').value;
-    if (!inptwidth) {
-        inptwidth = 3;
-    }
 
     rctx.lineWidth = inptwidth;
     rctx.lineCap = 'round';
@@ -24,41 +23,61 @@ function draw() {
 
     rctx.beginPath();
 
-    cQ = (smooth+1)/4*2-1;
-    cR = (smooth+1)/4*3-1;
+    //Chaikin's Algorithm
+    var points = [[],[]]
+    //points[0] == X
+    //      [1] == Y
+    
+    mouseX = chaikinsh(points[0],mouseX)
+    mouseY = chaikinsh(points[1],mouseY)
 
-    //Chaikin's Algorithm + BezierCurve (Smoothing Algorithm)
-    mouseX[cQ] = (mouseX[0] * (3/4)) + (mouseX[smooth] * (1/4))
-    mouseX[cR] = (mouseX[0] * (1/4)) + (mouseX[smooth] * (3/4))
-    mouseY[cQ] = (mouseY[0] * (3/4)) + (mouseY[smooth] * (1/4))
-    mouseY[cR] = (mouseY[0] * (1/4)) + (mouseY[smooth] * (3/4))
+    rctx.moveTo(nextline[0], nextline[1]);
+    rctx.quadraticCurveTo(points[0][1], points[1][1], points[0][0], points[1][0])
 
-    rctx.moveTo(mouseX[0], mouseY[0]);
-    rctx.bezierCurveTo(mouseX[cQ], mouseY[cQ], mouseX[cR], mouseY[cR], mouseX[smooth], mouseY[smooth]);
+    rctx.moveTo(points[0][0], points[1][0]);
+    // rctx.lineTo(dcanvas.clientWidth*2, dcanvas.clientHeight/2);
 
-    // console.log(mouseX);
-    // console.log(mouseY);
+    nextline[0] = points[0][0];
+    nextline[1] = points[1][0];
 
-    rctx.closePath();
 
-    rctx.stroke();
-    display();
+    rctx.closePath()
+    rctx.stroke()
+    display()
+
+    resizestore = rctx.getImageData(0, 0, rwidth, rheight);
+
+    function chaikinsh(points, temp) {
+        points[0] = (temp[0] * (3/4)) + (temp[1] * (1/4))
+        points[1] = (temp[0] * (1/4)) + (temp[1] * (3/4))
+        
+        return temp;
+    }
 }
 
 // copy from rcanvas to dcanvas
 // var displaystore;
+display();
+var size = intsize;
 function display() {
     // displaystore = rctx.getImageData(0, 0, rwidth, rheight);
     // dctx.putImageData(displaystore, 0, 0);
-    var size = document.querySelector('#size').value;
-    console.log(size);
+    // size = document.querySelector('#size').value;
+    console.log(intsize)
     dctx.clearRect(0, 0, rwidth, rheight);
-    dctx.drawImage(rcanvas, rwidth, rheight, rwidth*size, rheight*size)
+    dctx.drawImage(rcanvas, 0, 0, rwidth*size, rheight*size);
 }
-
-document.querySelector('#size').addEventListener('input', (e) => {
+document.querySelector('#sizeL').addEventListener('click', (e) => {
+    size = size*2
+    intsize--;
     display();
 })
+document.querySelector('#sizeS').addEventListener('click', (e) => {
+    size = size/2
+    intsize++;
+    display();
+})
+
 
 // stamp different shapes
 function stamp(type, size) {
@@ -70,7 +89,7 @@ function stamp(type, size) {
             break;
     }
 }
-
+// stamp circle
 function circle(size) {
     color = document.querySelector('#pickcolour').value;
 
@@ -91,16 +110,18 @@ function circle(size) {
 
 
 // mouse tracking
-var smooth = 3*1;
-var mouseX = [smooth];
-var mouseY = [smooth];
+var mouseX = [];
+var mouseY = [];
 var mouseState = 0;
 window.addEventListener('mousemove', (e) => {
-    mouseX[smooth] = mouseX[0];
-    mouseY[smooth] = mouseY[0];
 
-    mouseX[0] = e.clientX;
-    mouseY[0] = e.clientY;
+    // inptwidth = document.querySelector('#inptwidth').value;
+    // stamp('circle', inptwidth);
+
+    // mouseState = 1;
+    //
+    mouseX[0] = e.clientX*(size/size/size)*(intsize/(intsize*0.5));
+    mouseY[0] = e.clientY*(size/size/size)*(intsize/(intsize*0.5));
 
     // for (var i=3;i>0;i--) {
     //     mouseX[i] = mouseX[i-1];
@@ -111,38 +132,51 @@ window.addEventListener('mousemove', (e) => {
     if(mouseState == 1) {
         draw();
     }
+
+    mouseX[1] = mouseX[0];
+    mouseY[1] = mouseY[0];
+    
 })
 window.addEventListener('mousedown', (e) => {
     inptwidth = document.querySelector('#inptwidth').value;
-    if (!inptwidth) {
-        inptwidth = 3;
-    }
     stamp('circle', inptwidth);
+
+    nextline[0] = mouseX[0];
+    nextline[1] = mouseY[0];
+
     mouseState = 1;
 })
 window.addEventListener('mouseup', (e) => {
     mouseState = 0;
 })
 
+
 // resize
 var resizestore;
+resizestore = rctx.getImageData(0, 0, rwidth, rheight);
 window.addEventListener('resize', (e) => {
-    resizestore = rctx.getImageData(0, 0, rwidth, rheight);
+    // resizestore = rctx.getImageData(0, 0, rwidth, rheight);
     resize();
     rctx.putImageData(resizestore, 0, 0);
     display();
 })
+
 function resize() {
-    rwidth = rcanvas.clientWidth;
-    rheight = rcanvas.clientHeight;
+    rwidth = dcanvas.clientWidth*intsize;
+    rheight = dcanvas.clientHeight*intsize;
     rcanvas.width = rwidth;
     rcanvas.height = rheight;
     dcanvas.width = rwidth;
     dcanvas.height = rheight;
 }
 
+
 // clear everything
 function trash() {
     rctx.clearRect(0, 0, rwidth, rheight);
     display();
+}
+
+function drawoff() {
+    //smh make it so tht mousestate is constantly 0, refer to mousedown
 }
